c824d9bc2074971cb737de72303c0feaf8c27dfd
Kartheek Palepu <kartheekpnsn@gmail.com>
2017-01-20 17:53:26 +0530

    Update logistic-regression.py

--- Python/gradient-descent/logistic-regression.py
+++ Python/gradient-descent/logistic-regression.py
@@ -5,7 +5,10 @@
 
 import math
 
-# # minimize the loss
+# # # Loss function = half-MSE # # #
+# # parameters
+# y = original values of target variable
+# yhat = predicted values of target variable
 def loss(y, yhat):
 	term1 = [y[i] * math.log(yhat[i]) for i in range(len(y))]
 	term2 = [(1 - y[i]) * math.log((1 - yhat[i])) for i in range(len(y))]
@@ -13,29 +16,52 @@ def loss(y, yhat):
 	term4 = -term3/float(len(y))
 	return term4
 
+# # # derivative function of intercept # # #
+# # parameters
+# y = original values of target variable
+# yhat = predicted values of target variable
 def derivative1(y, yhat):
 	d = sum([yhat[i] - y[i] for i in range(len(y))])
 	d = d / float(len(y))
 	return(d)
 
+# # # derivative function of slopes i.e. theta1, theta2, theta3.... thetaN # # #
+# # parameters
+# y = original values of target variable
+# yhat = predicted values of target variable
+# x_n = list of values of nth feature in the data
 def derivativeN(y, yhat, x_n):
 	d = sum([((yhat[i] - y[i]) * x_n[i]) for i in range(len(y))])
 	d = d / float(len(y))
 	return(d)
 
-# # sigmoid function
-def yEQ(x, intercept, slopes, flag = False):
-	mx = [[j * slopes[i] for j in x[i]] for i in range(len(x))]
-	mxc = [intercept + sum(i) for i in zip(*mx)]
-	if flag:
-		print mxc
+
+# # # A function that calculates the derivative change with learning rate # # #
+# # parameters
+# x = list of lists of independent feature values in the data
+# y = original values of target variable
+# yhat = predicted values of target variable
+# params = parameters that needs optimization
+# learning_rate = learning_rate to proceed with the gradient
+def derivatives(x, y, yhat, params, learning_rate):
+	params['intercept'] = round(params['intercept'] - learning_rate * derivative1(y, yhat), 6)
+	params['slopes'] = [round(params['slopes'][i] - learning_rate * derivativeN(y, yhat, x[i]), 6) for i in range(len(x))]
+	return(params)
+
+# # # A function (sigmoid function) that calculates y value given x and params # # #
+# # parameters
+# x = list of lists of independent feature values in the data
+# params = parameters that needs optimization
+def yEQ(x, params):
+	mx = [[j * params['slopes'][i] for j in x[i]] for i in range(len(x))]
+	mxc = [params['intercept'] + sum(i) for i in zip(*mx)]
 	yhat = [(1/ (1 + math.exp(-i))) for i in mxc]
 	yhat = [0.9999999999999999 if i == 1.0 else i for i in yhat]
 	return yhat
 
-# # hyper parameters
-alpha = 0.01
-threshold = 0.001
+
+# # import gradient (vanilla_gradient_descent) algorithm
+from gradient import vanilla_gradient_descent
 
 # # initializations
 # # generate x
@@ -50,28 +76,7 @@ x.append(x3)
 # # formulate original 'y' i.e. y = 1/(1 + exp(-(1 + x1 + 2*x2 + x3)))
 slopes = [1, -2, 1]
 intercept = 1
-y = [round(i) for i in yEQ(x, intercept = intercept, slopes = slopes)]
+y = [round(i) for i in yEQ(x, params = {'intercept' : intercept, 'slopes' : slopes})]
 
-# # initialize values
-slopes = [0] * len(x)
-intercept = 0
-
-# # start iterations
-for i in range(10000000):
-	yhat = yEQ(x, intercept = intercept, slopes = slopes)
-	if loss(y, yhat) <= threshold:
-		print "=============================================================================="
-		print "=============================================================================="
-		print "== Converged at " + str(i) + " =="
-		print "- Slopes = " + ",".join([str(k) for k in slopes])
-		print "- Intercept = " + str(intercept)
-		print "- Y values = " + ", ".join([str(k) for k in y])
-		print "- Yhat values = " + ", ".join([str(k) for k in yhat])
-		print "- Loss = " + str(loss(y, yhat))
-		break;
-	if i % 1000 == 0:
-		print "FINISHED ITERATION # " + str(i)
-		print "At Slopes [" + ", ".join([str(k) for k in slopes]) + "] and intercept = " + str(intercept) + " loss is " + str(loss(y, yhat))
-	intercept = round(intercept - alpha * derivative1(y, yhat), 6)
-	for j in range(len(slopes)):
-		slopes[j] = round(slopes[j] - alpha * derivativeN(y, yhat, x[j]), 6)
+# # run vanilla_gradient_descent
+vanilla_gradient_descent(x, y, learning_rate = 0.01, print_every = 1000, threshold = 0.001, iters = 10000000, params = {'slopes' : [0] * len(x), 'intercept' : 0}, derivatives = derivatives, yEQ = yEQ, loss = loss)
