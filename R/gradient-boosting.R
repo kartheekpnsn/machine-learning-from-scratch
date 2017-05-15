a04fe02fe68c80bd770c777f69abce805b72589a
Kartheek Palepu <kartheekpnsn@gmail.com>
2017-05-15 16:39:35 +0530

    Update gradient-boosting.R

--- R/gradient-boosting.R
+++ R/gradient-boosting.R
@@ -15,51 +15,93 @@
 			# # y_pred - gradient
 	# # squash it using soft max
 
-f_gradientfit = function(data, target, learning_rate = 0.5, n_estimators = 10, verbose = FALSE) {
-	y_pred = rep(mean(data[, target]), nrow(data))
+f_gradient_fit = function(data, target = 'Y', base_model = NULL, n_estimators = 100, learning_rate = 1, verbose = TRUE) {
+	nclasses = length(unique(data[, target]))
 	y = data[, target]
-	fit_list = list()
-	library(rpart)
-	for(i in 1:n_estimators) {
-		gradient = (1 / (1 + exp(-y_pred))) - y
-		m_data = cbind(subset(data, select = setdiff(colnames(data), target)), Y = gradient)
-		sample_m_data = m_data[sample(rownames(m_data), round(nrow(m_data) * 0.7)), ]
-		fit = rpart(Y ~ ., data = sample_m_data, control = rpart.control(minsplit = 2, maxdepth = 2))
-		fit_list[[i]] = fit
-		update = predict(fit, m_data)
-		y_pred = y_pred - (learning_rate * update)
-		progress = 100 * (i / n_estimators)
-		if(progress %% 10 == 0) {
-			print(paste('Progress at tree -', i, ':', progress, "%"))
-			error = (1/length(y_pred)) * sum(((1 / (1 + exp(-y_pred))) - y)^2)
-			print(paste0("Error at tree - ", i, ": ", error))
-			if(verbose) {
-				print(cbind(gradient = gradient, y_pred = y_pred))
+	model_list = list()
+	for(eachClass in 1:nclasses) {
+		print(paste("# # For Class:", eachClass - 1, "# #"))
+
+		if(is.null(base_model)) {
+			y_pred = rep(mean(y), nrow(data))
+		} else {
+			y_pred = predict(base_model, data, type = 'prob')[, eachClass]
+		}
+
+		fit_list = list()
+		for(i in 1:n_estimators){
+			gradient = -(y - y_pred)
+			m_data = cbind(subset(data, select = setdiff(colnames(data), target)), Y = gradient)
+			fit = rpart(Y ~ ., data = m_data, control = rpart.control(minsplit = 2, maxdepth = 2))
+			fit_list[[i]] = fit
+			update = predict(fit, m_data)
+			y_pred = y_pred - (learning_rate * update)
+			if(i %% 10 == 0) {
+				progress = (i/n_estimators) * 100
+				loss = (1/length(y_pred)) * sum((y - y_pred)**2)
+				if(verbose) {
+					print(paste("# # Progress :", progress, "# #"))
+					print(paste("# # Loss     :", loss, "# #"))
+				}
 			}
 		}
+		model_list[[eachClass]] = fit_list
 	}
-	return(fit_list)
+	return(model_list)
+}
+
+f_gradient_predict = function(model_list, newdata, learning_rate = 1, validate = TRUE, target = 'Y', base_model = NULL) {
+	y_pred = list()
+	for(eachClass in 1:length(model_list)) {
+		y_pred[[eachClass]] = rep(0, nrow(newdata))
+		for(tree in model_list[[eachClass]]) {
+			update = predict(tree, newdata = newdata)
+			update = learning_rate * update
+			y_pred[[eachClass]] = y_pred[[eachClass]] - update
+		}
+	}
+	y_pred = do.call('cbind', y_pred)
+	y_pred = data.frame(t(apply(y_pred, 1, function(x) 1 - (exp(x)/sum(exp(x))))))
+	colnames(y_pred) = paste('class_', 1:length(model_list))
+	if(validate) {
+		source('https://raw.githubusercontent.com/kartheekpnsn/machine-learning-codes/master/R/functions.R')
+		y_pred[, target] = newdata[, target]
+		cutoff = getCutoff(probabilities = y_pred[, 2], original = newdata[, target], plotROC = FALSE, all = FALSE)
+		print("# # Base model performance # #")
+		print(performance(predicted = predict(base_model, newdata), original = newdata[, target]))
+		print("# # Boosted model performance # #")
+		print(performance(predicted = as.numeric(y_pred[, 2] >= cutoff), original = newdata[, target]))
+		return(list(cutoff = cutoff, predicted = y_pred))
+	} else {
+		return(list(predicted = y_pred))
+	}
 }
 
 
-f_predict = function(fit, newdata) {
-	y_pred = rep(0, nrow(newdata))
-	for(i in 1:n_estimators) {
-		update = predict(fit[[i]], newdata)
-		update = learning_rate * update
-		y_pred = y_pred - upte
-		print(rbind(update, y_pred))
-	}
-	y_pred = exp(y_pred)/sum(exp(y_pred))
-	return(y_pred)
-}
+# # # # # IMPLEMENTATION # # # # # 
+	data = data.frame(A = sample(c('male', 'female', 'others'), 1000, replace = T), 
+						B = sample(c('normal', 'lean', 'fit', 'fat'), 1000, replace = T))
+	data$Y = 0
+	data$Y[data$A == 'male' & data$B %in% c('fat', 'lean', 'normal')] = 1
+	data$Y[data$A == 'female' & data$B %in% c('lean', 'fat')] = 1
+	data$Y[data$A == 'others' & data$B == 'lean'] = 1
 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # IMPLEMENTATION # # #
-df = data.frame(A = runif(100, 10, 50), B = runif(100, 20, 100))
-df$Y = c(0, 1, 1, 0)
+	test = data.frame(A = sample(c('male', 'female', 'others'), 100, replace = T), 
+						B = sample(c('normal', 'lean', 'fit', 'fat'), 100, replace = T))
+	test$Y = 0
+	test$Y[test$A == 'male' & test$B %in% c('fat', 'lean', 'normal')] = 1
+	test$Y[test$A == 'female' & test$B %in% c('lean', 'fat')] = 1
+	test$Y[test$A == 'others' & test$B == 'lean'] = 1
 
-fit = f_gradientfit(df, target = 'Y')
+	target = 'Y'
+	learning_rate = 1
+	rounds = 100
+	fit_list = list()
+	library(rpart)
+	library(randomForest)
+	base_model = randomForest(factor(Y) ~ ., data = data, ntree = 2, mtries = 1)
+	table(predict(base_model, data), data$Y)
 
-f_predict(fit, df)
+
+	model_list = f_gradient_fit(data = data, target = 'Y', base_model = base_model, n_estimators = 10)
+	predicted = f_gradient_predict(model_list, newdata = test, target = 'Y', validate = TRUE, base_model = base_model)
